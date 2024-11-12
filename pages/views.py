a0cb0cfaf7fd771bd3a910918a95c5fb4db6c68b
-angular-localgit <-angular-localgit>
2024-11-12 14:11:43 +0000

    Add

--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home_page_view(request):
     return HttpResponse("Hello, i am gaurav!")
+
+def about_page_view(request):  # new
+    return render(request, "about.html")
