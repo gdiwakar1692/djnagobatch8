58e656e2f62ce155adfb4b13be2d4f0d8416a2fa
-angular-localgit <-angular-localgit>
2024-11-13 09:47:07 +0000

    modify html file

--- pages/views.py
+++ pages/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_page_view(request):
-    return HttpResponse("Hello, i am gaurav!")
+    return HttpResponse("Hello Gaurav , i am on the earth!")
 
 def about_page_view(request):  # new
     return render(request, "about.html")
